5a55e59e19164ad55d73a01f71259e1b68874e3c
Leopoldo José Cálix Parada <leopoldoj.calix@gmail.com>
2024-01-22 14:57:03 -0600

    Resolviendo LAB4-1

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
 def calcular_promedios(verbales: str, numericas: str) -> int:
     pass
+#Funcion que calcula el promedio de los puntajes verbales y numericos
+
+def calcular_promedios(numerical_skills, verbal_skills):
+    numerical_skills = list(map(int, numerical_skills.split()))
+    verbal_skills = list(map(int, verbal_skills.split()))
+
+    averages = [(n + v) / 2 for n, v in zip(numerical_skills, verbal_skills)]
+    general_average = sum(averages) / len(averages)
+
+    return sum(1 for avg in averages if avg < general_average)
